9bc387e5c854ccdc949c43105e4b2c8145a7fe62
larshp <larshp@hotmail.com>
2019-07-19 14:52:52 +0200

    fix

--- script/pages/stats.js
+++ script/pages/stats.js
@@ -29,16 +29,7 @@ class Issues {
         <section>`;
 
       for (const issue of sorted[type]) {
-        const match = issue.file.match(owner + "\/" + repo + "\/(.*)");
-        if (match) {
-          // todo, replace this part
-          // this part can be deleted after next regeneration of dataset
-          match[1] = match[1].replace(/#/g, "%23");
-          html = html + "<a href=\"https://github.com/" + owner + "/" + repo + "/blob/master/" + match[1] + "#L" + issue.start.row + "\">" + escape(issue.description) + "</a><br>";
-        } else {
-// new code
-          html = html + linkFile(owner + "/" + repo, issue.file, issue.start.row);
-        }
+        html = html + linkFile(owner + "/" + repo, issue.file, issue.start.row);
       }
 
       html = html + `</section></div>`;
